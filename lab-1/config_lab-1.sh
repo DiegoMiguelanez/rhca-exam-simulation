5f9b2fe0a1e4f070b3638ef5cd5793b536e39dcb
DiegoMiguelanez <miguelanez.diego@gmail.com>
2020-03-31 07:11:07 -0400

    +lab-1_v0.1

---
+++ lab-1/config_lab-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+user=$1
+
+change_user_password ( ) {
+	#List of different combinations of characters that we're gonna mix for our new secret password
+	local list=( egUFkBvvG evJfUQL evJfUQLs bk9PHTX3 zA jrn 7zH zAjrn7zH6J bCwzUA 5dCW5YYT 5dCW 5YYTbX 7uPT3A)
+
+	#Generate our own password from a custom list
+	#a. Store a random element from the previous list in variables random1 and random2
+        local random1=$(( $RANDOM % ${#list[@]} ))
+        local random2=$(( $RANDOM % ${#list[@]} ))
+	#b.Mix our two recently created strings in a variable called password
+        local password="${list[$random1]}${list[$random2]}"
+	
+	#For testing purposes were going to print our password via stdout
+	echo "Nueva pass: $password"
+
+	#Change the user's actual password
+	echo $passord | passwd $user --stdin
+}
+
+change_user_password
