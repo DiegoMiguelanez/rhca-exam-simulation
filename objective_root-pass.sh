1ec7e39e88203d4e7785f5e6063fa3e1baa6e6a7
Diego R. Lopez Miguelanez <miguelanez.diego@gmail.com>
2020-03-31 10:37:20 +0200

    "Nueva palabra al diccionario"

--- objective_root-pass.sh
+++ objective_root-pass.sh
@@ -2,7 +2,7 @@
 
 user=$1
 
-list=( ja Ka ma Sa Rex TO per stack 23 234 52 sok q2 qdf 23 SER3wr 243 )
+list=( RESURRECION ja Ka ma Sa Rex TO per stack 23 234 52 sok q2 qdf 23 SER3wr 243 )
 
 #Generate our own password from a custom list
 generate_password () {
